3b7d3a09ca8edefc86bc17516c5f74f893c87277
sakit333 <sak528264@gmail.com>
2024-09-11 10:21:16 +0530

    updated with input from JSON

---
+++ src/main/java/sakbuilder/HelloLambda.java
@@ -0,0 +1,8 @@
+package sakbuilder;
+
+public class HelloLambda {
+    
+    public String handleRequest(String name) {
+        return "hello" + name + "!";
+    }
+}
---
+++ src/test/java/sakbuilder/HelloLambdaTest.java
@@ -0,0 +1,12 @@
+package sakbuilder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class HelloLambdaTest {
+    @Test
+    void testHandleRequest() {
+        var sut = new HelloLambda();
+        assertEquals("hello!", sut.handleRequest(""));
+    }
+}
